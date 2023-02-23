13b4481eafccd29c928c21413c8515600f9daaa8
mohammadqasimmatloob <Qasimmatloob786@gmail.com>
2023-02-23 12:27:07 +0000

    Update DistanceConverter.cs
    Added empty Methods and attributes and added description of basic  features

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -4,11 +4,51 @@ namespace ConsoleAppProject.App01
 {
     /// <summary>
     /// Please describe the main features of this App
+    /// (1) Output a heading with the name of the application and the name of the programmer
+    /// (2) Input any valid distance measured in miles
+    /// (3) Convert that distance into feet
+    /// (4) Output the equivalent distance in feet
+    /// (5) Input a distancedistancein of feet and convert it to miles
+    /// (6) Input a distance in miles and convert it to metres 
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Mohammad Qasim Matloob 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet; 
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number.
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CalculateFeet()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OutputFeet()
+        {
+
+        }
     }
 }
